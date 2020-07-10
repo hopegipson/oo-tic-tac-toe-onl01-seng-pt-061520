e4b261632867e29f6a070eefec170bb80df8e2b7
Hope Gipson <gipson.hope@gmail.com>
2020-07-10 23:29:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,9 +89,9 @@ WIN_COMBINATIONS = [
      
     def winner
    WIN_COMBINATIONS.detect do |win_combo|
-     if (@board[win_combo[0]]) == "X"
+     if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
        return "X"
-     elsif (@board[win_combo[0]]) == "O"
+     elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" &&   (@board[win_combo[2]]) == "O"
        return "O"
       else
        nil
