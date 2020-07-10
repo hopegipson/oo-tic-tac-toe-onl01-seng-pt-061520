469b6b7886faad21cc37983aef16de379604eeb0
Hope Gipson <gipson.hope@gmail.com>
2020-07-10 23:31:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,9 +100,15 @@ WIN_COMBINATIONS = [
   end
   
   def play
-    
-    
+  while over? == false
+    turn
   end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
   
 
 end
